3b06aee11f48cb801c26d246f5424de15dfaa4d7
PanhaOeun <71807132+panhaoeun@users.noreply.github.com>
2023-02-15 17:22:42 +0700

    update

--- src/services/authencationDataService.js
+++ src/services/authencationDataService.js
@@ -1,5 +1,9 @@
 import http from "../../http-common";
 class AuthenticationsDataService{
+    // Login 
+    authLogin(data){
+        return http.post("/auth/signIn", data);
+    }
     create(data){
         return http.post("/auth/signUp", data);
     }
--- src/views/authencation/opt_verify/OPTVerify.vue
+++ src/views/authencation/opt_verify/OPTVerify.vue
@@ -7,46 +7,41 @@
                   <img src="../../../assets/logo/ecommerce.png" class="w-1 text-center"/>
               </a>
           </div>
-          <div class="flex align-items-center justify-content-center overflow-hidden">
-              <div class="flex flex-column align-items-center justify-content-center">
-                  <div>
-                      <div class="w-full surface-card py-8 px-5 sm:px-2 w-full" style="border-radius: 53px"> 
-                          <!-- Form Submit -->
-                          <div class="card">
-                                <form @submit.prevent="handleSubmitDigitPhoneVerify()">
-                                    <div class="py-2">
-                                        <!-- Text -->
-                                        <div class="text-center mb-5">
-                                            <div class="text-900 text-3xl font-medium mb-3">Verify mobile number</div>
-                                            <span class="text-600 font-medium">A text with a One Time Password (OTP) has been sent to your mobile number</span>
-                                        </div>
-                                        <!--Input Verify OPT -->
-                                        <div class="col-6 md:col-6 ">
-                                            <label for="opt-verify-input">Enter OTP:</label>
-                                            <span class="p-input-icon-right">
-                                                    <InputMask id="opt-verify-input" class="p-inputtext-lg" style="height: 50;" mask="9-9-9-9-9-9" v-model="valInputVerifyOPT" placeholder="Enter 6 Digit" />
-                                                    <i class="pi pi-spin pi-spinner" />
-                                            </span>
-                                          
-                                        </div>
-        
-                                        <!-- Create account button -->
-                                        <div class="flex justify-content-center">
-                                            <Button type="submit" label="Continue" :loading="isLoading" class="mt-2 p-button-rounded p-button-md" style="font-size: 16px; color: white;width: 250px; height: 50px;"/>
-                                        </div>
-                                    </div>
-                                </form>
-                          </div>  
-                          <!-- Form Submit -->                    
-                      </div>
-                  </div>
-              </div>
+          <div class="flex align-items-center justify-content-center overflow-hidden px-4 py-4">
+                <!-- Form Submit -->
+                <div class="card">
+                    <form @submit.prevent="handleSubmitDigitPhoneVerify()">
+                        <div class="py-2 px-3">
+                            <!-- Text -->
+                            <div class="text-center mb-5">
+                                <div class="text-900 text-3xl font-medium mb-3">Verify mobile number</div>
+                                <span class="text-600 font-medium">A text with a One Time Password (OTP) has been sent to your mobile number</span>
+                            </div>
+                        </div>
+                           <!--Input Verify OPT -->
+                            <div class="col-6 md:col-6">
+                                <label for="opt-verify-input">Enter OTP:</label>
+                                <span class="p-input-icon-right">
+                                    <InputMask id="opt-verify-input" class="p-inputtext-lg p-fluid" style="height: 50px; width: 40rem;" mask="9-9-9-9-9-9" v-model="valInputVerifyOPT" placeholder="Enter 6 Digit" />
+                                    <i class="pi pi-spin pi-spinner" />
+                                </span>
+                                
+                            </div>
+                            <!-- Create account button -->
+                            <div class="flex justify-content-center">
+                                <Button type="submit" label="Continue" :loading="isLoading" class="mt-2 p-button-rounded p-button-md" style="font-size: 16px; color: white;width: 300px; height: 50px;"/>
+                            </div>
+                        <div class="px-4 py-4">
+                            By creating an account, you agree to PzharKhmer Conditions of Use and Privacy Notice.
+                        </div>
+                    </form>
+                </div>  
+                <!-- Form Submit -->        
           </div>
       </div>
     </div>
-    
-      <!-- Footer for Auth Login -->
-      <footer class="fixed-bottom w-full layout-footer index-1 w-full text-500 p-2 footer" style="background-color: #333;">
+    <!-- Footer for Auth Login -->
+    <footer class="fixed-bottom w-full layout-footer index-1 w-full text-500 p-2 footer" style="background-color: #333;">
           <div class="container text-500">
               <a href="#" class="no-underline hover:underline text-white hover:text-500" ref="nofollow">Intellectual Property Protection</a> - 
               <a href="" class="no-underline hover:underline text-white hover:text-500" ref="nofollow">Privacy Policy</a> 
@@ -56,7 +51,6 @@
               ©️ {{ Date('YYYY') }} PzharKhmer.com. All rights reserved.
           </div>
       </footer>
-      <!-- <AppConfig simple /> -->
   </template>
   
   <!-- Input Verify OPT - 5digit -->
@@ -64,12 +58,13 @@
         export default{
             data(){
                 return {
+                    isLoading: false,
                     valInputVerifyOPT: ''
                 }
             },
             methods: {
                 async handleSubmitDigitPhoneVerify(){
-                    console.log("dfdf")
+                    console.log(this.valInputVerifyOPT)
                 }
             }
         }
