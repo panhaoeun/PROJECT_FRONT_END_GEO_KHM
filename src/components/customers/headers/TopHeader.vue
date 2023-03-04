7e1da54e2ccd9cc6af88e306dc0e6ff9145ad43f
trypuoyy <puoyymu2014@gmail.com>
2023-03-04 20:43:45 +0700

    new nav bar

--- src/components/customers/headers/TopHeader.vue
+++ src/components/customers/headers/TopHeader.vue
@@ -1,7 +1,121 @@
-<template> 
-   <div class="flex justify-content-right">
-        <p>Top header</p>
-   </div>
+<template>
+    <div class="d-none d-lg-block">
+        <div class="flex justify-content-center align-item-center">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown">
+                            <a
+                                class="nav-link dropdown-toggle"
+                                href="#"
+                                id="navbarDropdownMenuLink"
+                                data-toggle="dropdown"
+                                aria-haspopup="true"
+                                aria-expanded="false"
+                            >
+                                Sell on our website
+                            </a>
+                            <div
+                                class="dropdown-menu"
+                                aria-labelledby="navbarDropdownMenuLink"
+                            >
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#"
+                                    >Another action</a
+                                >
+                                <a class="dropdown-item" href="#"
+                                    >Something else here</a
+                                >
+                            </div>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a
+                                class="nav-link dropdown-toggle"
+                                href="#"
+                                id="navbarDropdownMenuLink"
+                                data-toggle="dropdown"
+                                aria-haspopup="true"
+                                aria-expanded="false"
+                            >
+                                Help
+                            </a>
+                            <div
+                                class="dropdown-menu"
+                                aria-labelledby="navbarDropdownMenuLink"
+                            >
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#"
+                                    >Another action</a
+                                >
+                                <a class="dropdown-item" href="#"
+                                    >Something else here</a
+                                >
+                            </div>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Buyer Protection</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Wish List</a>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a
+                                class="nav-link dropdown-toggle"
+                                href="#"
+                                id="navbarDropdownMenuLink"
+                                data-toggle="dropdown"
+                                aria-haspopup="true"
+                                aria-expanded="false"
+                            >
+                                English / KHR
+                            </a>
+                            <div
+                                class="dropdown-menu"
+                                aria-labelledby="navbarDropdownMenuLink"
+                            >
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#"
+                                    >Another action</a
+                                >
+                                <a class="dropdown-item" href="#"
+                                    >Something else here</a
+                                >
+                            </div>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a
+                                class="nav-link dropdown-toggle"
+                                href="#"
+                                id="navbarDropdownMenuLink"
+                                data-toggle="dropdown"
+                                aria-haspopup="true"
+                                aria-expanded="false"
+                            >
+                                Account
+                            </a>
+                            <div
+                                class="dropdown-menu"
+                                aria-labelledby="navbarDropdownMenuLink"
+                            >
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#"
+                                    >Another action</a
+                                >
+                                <a class="dropdown-item" href="#"
+                                    >Something else here</a
+                                >
+                            </div>
+                        </li>
+                       
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
 </template>
 
 <script></script>
