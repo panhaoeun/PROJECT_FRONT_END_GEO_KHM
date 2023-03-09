13212e8d8b5c284f8f85b0cb36b61525e9b25633
PanhaOeun <71807132+panhaoeun@users.noreply.github.com>
2023-03-09 22:29:57 +0700

    updated

--- src/main.js
+++ src/main.js
@@ -9,6 +9,18 @@ import { far } from '@fortawesome/free-regular-svg-icons';
 import {faLock, faEnvelope} from '@fortawesome/free-solid-svg-icons';
 import {faFacebook, faGooglePlus} from '@fortawesome/free-brands-svg-icons';
 
+//import adminlte styles
+import './assets/styles/adminlte.min.css'
+import "../node_modules/admin-lte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css"
+import "../node_modules/admin-lte/plugins/summernote/summernote-bs4.min.css"
+import "../node_modules/admin-lte/plugins/daterangepicker/daterangepicker.css"
+import "../node_modules/admin-lte/plugins/jqvmap/jqvmap.min.css"
+import "../node_modules/admin-lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css"
+import "../node_modules/admin-lte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css"
+import "../node_modules/admin-lte/plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css"
+import "../node_modules/admin-lte/plugins/bs-stepper/css/bs-stepper.min.css"
+import "../node_modules/admin-lte/plugins/dropzone/min/dropzone.min.css";
+
 //import adminlte scripts
 import "../node_modules/admin-lte/dist/js/adminlte.min.js"
 import "../node_modules/admin-lte/plugins/select2/js/select2.full.min.js"
