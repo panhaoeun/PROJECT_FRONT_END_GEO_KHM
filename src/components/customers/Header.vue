e8535daff7dd6ac3c9798ec5843e96d3b872b227
PanhaOeun <71807132+panhaoeun@users.noreply.github.com>
2023-03-10 07:38:15 +0700

    updated

--- src/components/customers/Header.vue
+++ src/components/customers/Header.vue
@@ -1,8 +1,8 @@
 <template>
     <TopHeader />
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">E-commerce</a>
+    <nav class="ml-sm-5 navbar navbar-expand-lg navbar-light bg-light" id="app">
+        <a class="navbar-brand ml-sm-5" href="#">E-commerce</a>
         <button
             class="navbar-toggler"
             type="button"
@@ -15,54 +15,56 @@
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                
+        <div
+            class="collapse navbar-collapse ml-sm-5 mb-2"
+            id="navbarSupportedContent"
+        >
+            <div class="w-100 d-flex flex-column">
+                <div>
+                    <form class="form-inline my-2 my-lg-0 w-100">
+                        <input
+                            class="form-control mr-sm-2 w-50"
+                            type="search"
+                            placeholder="Search"
+                            aria-label="Search"
+                        />
+                        <button
+                            class="btn btn-outline-danger my-2 my-sm-0"
+                            type="submit"
+                        >
+                            <i class="pi pi-search"></i>
+                        </button>
+                        <div>
+                            <a href="#" class="ml-5">
+                                <font-awesome-icon icon="fa-solid fa-shopping-cart" size="lg" />
+                            </a>
+                        </div>
+                    </form>
+                </div>
+
+                <div class="d-sm-flex gap-2 -mt-2 d-none">
+                    <a href="#" class="px-1 py-0 border-right"
+                        >sunglasses for men</a
+                    >
+                    <a href="#" class="px-1 py-0 border-right">iphone case</a>
+                    <a href="#" class="px-1 py-0 border-right"
+                        >i phone 12 pro max mobile</a
+                    >
+                    <a href="#" class="px-1 py-0 border-right"> nails</a>
+                    <a href="#" class="px-1 py-0 border-right"
+                        >plush car seat cover cushion</a
+                    >
+                    <a href="#" class="px-1 py-0">rings</a>
+                </div>
+            </div>
+
+            <!-- <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
                     <a class="nav-link" href="#"
-                        ><i class="shopping-cart-icon pi pi-shopping-cart"></i></a
-                    >
+                        ><i class="shopping-cart-icon pi pi-shopping-cart"></i
+                    ></a>
                 </li>
-
-                <li class="nav-item dropdown">
-                    <a
-                        class="nav-link dropdown-toggle"
-                        href="#"
-                        id="navbarDropdown"
-                        role="button"
-                        data-toggle="dropdown"
-                        aria-haspopup="true"
-                        aria-expanded="false"
-                    >
-                        Recommendation
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Car</a>
-                        <a class="dropdown-item" href="#">Smart Phones</a>
-                        <a class="dropdown-item" href="#">Watch for men</a>
-                        <a class="dropdown-item" href="#">Headphones</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#"
-                            >Clothes</a
-                        >
-                    </div>
-                </li>
-            
-            </ul>
-            <form class="form-inline my-2 my-lg-0 w-100">
-                <input
-                    class="form-control mr-sm-2 w-50"
-                    type="search"
-                    placeholder="Search"
-                    aria-label="Search"
-                /> 
-                <button
-                    class="btn btn-outline-danger my-2 my-sm-0"
-                    type="submit"
-                >
-                    <i class="pi pi-search"></i>
-                </button>
-            </form>
+            </ul> -->
         </div>
     </nav>
 </template>
@@ -79,5 +81,6 @@ export default {
     components: {
         TopHeader,
     },
+    name: 'App'
 };
 </script>
