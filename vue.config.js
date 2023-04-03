5e6669aefd11b4f67d0e31ec99cbda7191cbd54b
PanhaOeun <71807132+panhaoeun@users.noreply.github.com>
2023-04-03 09:11:28 +0700

    update

--- vue.config.js
+++ vue.config.js
@@ -1,27 +1,20 @@
-const Dotenv = require('dotenv-webpack');
-const envPath = function() {
-    return (!process.env.NODE_ENV || (process.env.NODE_ENV === 'development')) ?
-        './.env' :
-        `./.env.${process.env.NODE_ENV}`;
-}
-const dotenvArgs = {
-    expand: true,
-    path: envPath()
-};
 
 module.exports = {
    //... some other config here
    devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:8080',
         changeOrigin: true,
       },
     },
   },
-    configureWebpack: {
-        plugins: [
-            new Dotenv(dotenvArgs)
-        ]
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/' + process.env.CI_PROJECT_NAME + '/'
+    : '/',
+     pwa: {
+      workboxOptions: {
+        exclude: [/_redirects/]
+      }
     }
 };
