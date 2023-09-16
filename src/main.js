bc58eb2dd2cdae5639e009939aacf6331d5df230
PanhaOeun <71807132+panhaoeun@users.noreply.github.com>
2023-09-16 11:31:59 +0700

    updaded module

--- src/main.js
+++ src/main.js
@@ -208,7 +208,7 @@ app.component('RadioButton', RadioButton);
 app.component('Textarea', Textarea);
 app.use(ConfirmationService);
 app.component('FileUpload', FileUpload);
-app.component('ConfirmDialog', g);
+app.component('ConfirmDialog', ConfirmDialog);
 //Vue Uoloads
 app.component('file-upload', VueUploadComponent);
 app.component('TreeTable', TreeTable);
