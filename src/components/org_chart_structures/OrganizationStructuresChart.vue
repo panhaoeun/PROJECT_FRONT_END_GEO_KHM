534467aa280afb34abcb22623fcf8661facc6ff7
PanhaOeun <71807132+panhaoeun@users.noreply.github.com>
2024-01-24 11:07:08 +0700

    updated

---
+++ src/components/org_chart_structures/OrganizationStructuresChart.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>sadsad</div>
+</template>
+<!-- Organization Chart (Manage Org.structures) -->
+<script>
+export default {
+    components: {},
+    props: {},
+    data() {
+        return {};
+    },
+    created() {},
+    methods: {},
+    mounted() {},
+};
+</script>
+<style scoped>
+</style>
+<style lang='scss' scoped>
+</style>
--- src/views/administrators/user_managements/role_permission_module_new/manage_org_strictures/GlobalOrgChartOfGeoDeptPost.vue
+++ src/views/administrators/user_managements/role_permission_module_new/manage_org_strictures/GlobalOrgChartOfGeoDeptPost.vue
@@ -1,34 +1,24 @@
-
 <template>
-    <OrganizationChart v-model:selectionKeys="selection" :value="orgData" collapsible selectionMode="multiple">
-        <template #person="slotProps">
-            <div class="flex flex-column">
-                <div class="flex flex-column align-items-center">
-                    <img :alt="slotProps.node.data.name" :src="slotProps.node.data.image" class="mb-3 w-3rem h-3rem" />
-                    <span class="font-bold mb-2">{{ slotProps.node.data.name }}</span>
-                    <span>{{ slotProps.node.data.title }}</span>
-                </div>
-            </div>
-        </template>
-        <template #default="slotProps">
-            <span>{{ slotProps.node.label }}</span>
-        </template>
-    </OrganizationChart>
+    <OrganizationChartStructures />
 </template>
 
 <!-- Manage Global Org-Strictures Of Board Mgt Project-->
 <script>
+import OrganizationChartStructures from "@/components/org_chart_structures/OrganizationStructuresChart.vue";
 export default {
+    components: {
+        OrganizationChartStructures,
+    },
     props: {
         orgData: {
             type: Array,
             require: true,
-            default: () => []
-        }
-    },  
+            default: () => [],
+        },
+    },
     data() {
         return {
-            selection: {}
+            selection: {},
         };
     },
 };
